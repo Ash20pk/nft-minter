e3e2b4159a7c9e3e56a0fff5870805b330550348
Ash@metaschool <ash20pk@gmail.com>
2024-07-25 19:15:15 +0800

    Update hardhat.config.js

--- contract/hardhat.config.js
+++ contract/hardhat.config.js
@@ -1,7 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("@nomicfoundation/hardhat-verify");
 
-
 const dotenv = require("dotenv");
 dotenv.config();
 
@@ -13,8 +12,9 @@ module.exports = {
   networks: {
     core_testnet: {
       url: "https://rpc.test.btcs.network",
+      chainId: 1115,
       accounts: privateKey(),
-    }
+    },
   },
   solidity: {
     version: "0.8.24",
@@ -25,17 +25,21 @@ module.exports = {
         runs: 1000,
       },
     },
-  },  etherscan: {
-    apiKey: process.env.API_KEY, 
   },
-  customChains: [
-    {
-      network: "core_testnet",
-      chainId: 1115,
-      urls: {
-        apiURL: "https://api.test.btcs.network/api",
-        browserURL: "https://scan.test.btcs.network/"
+  etherscan: {
+    apiKey: process.env.API_KEY,
+    customChains: [
+      {
+        network: "core_testnet",
+        chainId: 1115,
+        urls: {
+          apiURL: "https://api.test.btcs.network/api", 
+          browserURL: "https://scan.test.btcs.network" 
+        }
       }
-    },
-  ]
+    ]
+  },
+  sourcify: {
+    enabled: true
+  }
 };
